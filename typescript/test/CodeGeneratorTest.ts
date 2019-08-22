d1207d16cda88d518b39d05d7daad9669e56a8d8
xiaozhenliugg <xiaozhenliu@google.com>
2019-08-22 12:07:51 -0700

    install google-gax

--- typescript/test/CodeGeneratorTest.ts
+++ typescript/test/CodeGeneratorTest.ts
@@ -12,7 +12,7 @@ const GENERATED_CLIENT_FILE = path.join(
     __dirname, '..', '..', 'showcase', 'tmp', 'src', 'v1beta1',
     'echo_client.ts');
 const GOOGLE_GAX_PROTOS_DIR = '-I/' +
-    path.join('usr', 'local', 'lib', 'node_modules', 'google-gax/protos');
+    path.join(__dirname, '..', '..', 'node_modules', 'google-gax/protos');
 const LOCAL_CLIENT_LIB_DIR =
     '-I' + path.join(__dirname, '..', '..', 'typescript', 'test', 'protos');
 const PROTO_DIR = path.join('google', 'showcase', 'v1beta1', 'echo.proto');
